0f305a11b50d6dd9753573ea6189086889ea230c
DHL68 <ldh1255@naver.com>
2022-07-14 13:25:22 +0900

    feat : user app 수정 #1 - admin.py 레지스트 추가 - models.py Rank model table 수정 - views.py from import 추가

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, UserProfile, UserCategory, Rank
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(UserProfile)
+admin.site.register(UserCategory)
+admin.site.register(Rank)
--- user/views.py
+++ user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import User as UserModel
+from .models import UserProfile as UserProfileModel
+from .models import UserCategory as UserCategoryModel
+from .models import Rank as RankModel
+
+
 # Create your views here.
