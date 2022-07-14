48e6498d8797046df4a6814b3ecaed989805f6a1
HWISU96 <soshyzx123@naver.com>
2022-07-14 13:48:08 +0900

    feat : article / serializers.py 추가 ArticleSerializer ArticleCreateSerializer

---
+++ article/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+
+from article.models import Article as ArticleModel
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArticleModel
+        fields= '__all__'
+
+
+class ArticleCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArticleModel
+        fields = '__all__'
+
+    farm_name = serializers.CharField(required=True, min_length=2)
+    title = serializers.CharField(required=True, min_length=4)
+
+    def create(self, validated_data):
+        article = ArticleModel.objects.create(
+            user_id=validated_data['user_id'],
+            article_category_id=validated_data['article_category_id'],
+            farm_name=validated_data['farm_name'],
+            location=validated_data['location'],
+            title=validated_data['title'],
+            cost=validated_data['cost'],
+            requirement=validated_data['requirement'],
+            period=validated_data['period'],
+            desc=validated_data['desc'],
+            img1=validated_data['img1'],
+            img2=validated_data['img2'],
+            img3=validated_data['img3'],
+            # exposure_end_date=validated_data['exposure_end_date'],
+            # display_article=validated_data['display_article'],
+        )
+        article.save()
+        return article
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data['title']
+        instance.save()
+        return instance
+
