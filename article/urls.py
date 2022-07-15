4c3f27a11cd3bae3da68abf41eaea8744547b9e8
HWISU96 <soshyzx123@naver.com>
2022-07-15 14:58:54 +0900

    feat : apply path 추가

--- article/urls.py
+++ article/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # article/
     path('detail/', views.ArticleDetailView.as_view()),
     path('detail/<article_id>', views.ArticleDetailView.as_view()),
+
+    path('detail/apply/<article_id>', views.ArticleApplyView.as_view()),
 ]
