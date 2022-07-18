02ba2d8528f5f365a65657cb44ebd453999935b5
imjuyoung <im_joo0@naver.com>
2022-07-18 14:47:34 +0900

    feat : 모델 네이밍 _id 제거,  마이페이지 view, url, seriallizer 작성 완료

--- article/urls.py
+++ article/urls.py
@@ -10,4 +10,14 @@ urlpatterns = [
     path('detail/<article_id>', views.ArticleDetailView.as_view()),
 
     path('detail/apply/<article_id>', views.ArticleApplyView.as_view()),
+
+    # user_category가 farm 인 경우의 mypage
+    path('farm/', views.FarmMyPageView.as_view(), name='farm_page'),
+    path('farm/<article_id>', views.FarmApplyView.as_view(), name='farm_apply_page'),
+
+    # user_category가 farmer 인 경우의 mypage ( naming 다시 )
+    path('farmer/', views.FarmerMyPageView.as_view(), name='farmer_page'),
+    path('<article_id>/farmer', views.FarmerMyPageView.as_view(), name='farmer_page'),
+    path('farmer/<review_id>', views.FarmerMyPageView.as_view(), name='farmer_page'),
+    path('review', views.FarmerReviewView.as_view(), name='farmer_review_page'),
 ]
