4cbd0c31e82df88ac68e55cbbdf43d0645615687
gyuhyeongH <ckacl792@gmail.com>
2022-07-27 12:48:04 +0900

    no message

--- first_farm_backend/settings.py
+++ first_farm_backend/settings.py
@@ -43,7 +43,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['15.165.159.220']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
