a45dc8c8c766fd8a75c1eb5f1f388805790c24d2
gyuhyeongH <ckacl792@gmail.com>
2022-07-28 19:56:44 +0900

    feat : reverse 쓰기 위해 url에 name설정

--- first_farm_backend/urls.py
+++ first_farm_backend/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
-    path('article/', include('article.urls'))
+    path('article/', include('article.urls'), name='article')
     ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
