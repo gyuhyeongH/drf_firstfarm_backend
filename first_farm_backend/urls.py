8d097914b94eed6ab876805373e0b6a2ef8bc315
gyuhyeongH <ckacl792@gmail.com>
2022-08-03 22:51:49 +0900

    fix : url 삭제

--- first_farm_backend/urls.py
+++ first_farm_backend/urls.py
@@ -24,5 +24,3 @@ urlpatterns = [
     path('user/', include('user.urls')),
     path('article/', include('article.urls'), name='article')
     ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
