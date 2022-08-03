ab657160fc16ad59ba22b778984f54b4fbe75637
DHL68 <ldh1255@naver.com>
2022-08-03 16:50:30 +0900

    feat : jwt custom 추가

--- user/jwt_claim_serializer.py
+++ user/jwt_claim_serializer.py
@@ -9,7 +9,11 @@ class FirstFarmTokenObtainPairSerializer(TokenObtainPairSerializer):
         token = super().get_token(user)
 
         # 사용자 지정 클레임 설정하기
+        token['id'] = user.id
+        token['username'] = user.username
+
         userprofile = UserProfile.objects.get(user=user)
         token['fullname'] = userprofile.fullname
+        token['category'] = user.user_category.id
 
         return token
