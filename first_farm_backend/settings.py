91753ad9a569aa8cdb86458b6a1efdee05323960
gyuhyeongH <ckacl792@gmail.com>
2022-08-16 13:24:51 +0900

    fix : 시간 수정

--- first_farm_backend/settings.py
+++ first_farm_backend/settings.py
@@ -219,7 +219,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
